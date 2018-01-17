038872b632a227c7eb58c657a985153b45b5dd2b
lw-like <wrobelllukasz@gmail.com>
2018-01-17 18:11:53 +0100

    Version UP 1.0.4

--- dist/pg-interval.js
+++ dist/pg-interval.js
@@ -15,7 +15,7 @@ var pgi = function (_interval) {
       ms: 0
     }
 
-    this.version = '1.0.3';
+    this.version = '1.0.4';
 
     this.updateValue = function updateValue () {
       __values.ms += this.getHours()   * 60 * 60 * 1000;
